14df049470604542f9d5bbbf70bcc098fdfdcaf9
Hedinyer Perucho <alucardperucho@gmail.com>
2024-10-14 12:51:40 +0000

    terraform file

--- s3/etags/main.tf
+++ s3/etags/main.tf
@@ -2,7 +2,22 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.71.0"
+      version = "5.30.0"
     }
   }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_s3_bucket" "default" {
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id
+  key    = "myfile.txt"
+  source = "myfile.txt"
+
+  etag = filemd5("myfile.txt")
 }
