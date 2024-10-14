58427f2af427241572c39a82cd4dc8e0dfaaacb6
Hedinyer Perucho <alucardperucho@gmail.com>
2024-10-14 12:41:39 +0000

    bash install files in ./bin

---
+++ bin/aws_cli_install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd /workspace
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+cd $THEIA_WORKSPACE_ROOT
